00407b042881613f36f726ef7fc1f4a158944558
yatochkaaa <jaffplay@gmail.com>
2023-06-12 12:55:52 +0300

    import fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../button/Button";
+import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 import "./Header.css";
 
